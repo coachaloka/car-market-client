7c135a4cbd36ee8879440ae3a606470ab8f9bfa0
Bachan-kubi <felixghagra@gmail.com>
2022-12-01 12:35:34 +0600

    logo added

--- src/Pages/Home/reviews/Reviewsproduct.js
+++ src/Pages/Home/reviews/Reviewsproduct.js
@@ -6,8 +6,9 @@ const Revewsproduct = () => {
             <div className="">
                 <section className="my-8 dark:bg-gray-800 dark:text-gray-100 pb-10">
                     <div className="container flex flex-col items-center mx-auto  md:p-10 md:px-12">
-                        <h1 className="p-4 text-4xl font-semibold leading-none text-center">Customers Feedback</h1>
+                        <h1 className="p-4 text-5xl font-semibold leading-none text-center text-cyan-700">Customers Feedback</h1>
                     </div>
+                    <hr />
                     <div className="container flex flex-col items-center justify-center mx-auto lg:flex-row lg:flex-wrap lg:justify-evenly lg:px-10">
                         <div className="flex flex-col max-w-sm mx-4 my-6 shadow-lg">
                             <div className="px-4 py-12 rounded-t-lg sm:px-8 md:px-12 dark:bg-gray-900">
@@ -53,7 +54,7 @@ const Revewsproduct = () => {
                                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" fill="currentColor" className="w-8 h-8 dark:text-violet-400">
                                         <path d="M232,246.857V16H16V416H54.4ZM48,48H200V233.143L48,377.905Z"></path>
                                         <path d="M280,416h38.4L496,246.857V16H280ZM312,48H464V233.143L312,377.905Z"></path>
-                                    </svg>The wedding is a ceremony where two people are united in marriage. Wedding traditions and customs vary greatly between cultures, ethnic groups, religions, countries,
+                                    </svg>We are very pleased to inform you about car points that to get the best qualities and services. Get involved with and try it out. 
                                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" fill="currentColor" className="absolute right-0 w-8 h-8 dark:text-violet-400">
                                         <path d="M280,185.143V416H496V16H457.6ZM464,384H312V198.857L464,54.1Z"></path>
                                         <path d="M232,16H193.6L16,185.143V416H232ZM200,384H48V198.857L200,54.1Z"></path>
@@ -62,8 +63,8 @@ const Revewsproduct = () => {
                             </div>
                             <div className="flex flex-col items-center justify-center p-8 rounded-b-lg dark:bg-violet-400 dark:text-gray-900">
                                 <img src="https://source.unsplash.com/50x50/?portrait?4" alt="" className="w-16 h-16 mb-2 -mt-16 bg-center bg-cover rounded-full dark:bg-gray-500 dark:bg-gray-700" />
-                                <p className="text-xl font-semibold leading-tight">Distinctio Animi</p>
-                                <p className="text-sm uppercase">Aliquam illum</p>
+                                <p className="text-xl font-semibold leading-tight">Epa Ghagra</p>
+                                <p className="text-sm uppercase">Rajshahi</p>
                             </div>
                         </div>
                     </div>
@@ -73,15 +74,16 @@ const Revewsproduct = () => {
 
 
                 <section className="dark:bg-gray-800 dark:text-gray-100 lg:px-20 pb-10">
-                    <h2 className="text-3xl font-bold text-center py-5 ">Ours Resell Car Buy To Company</h2>
-                    <div className="flex justify-center">
-                        <img className='w-10 md:w-16  mx-4 ' src="https://www.thegoodweddingcompany.com/wp-content/uploads/2019/03/the_good_wedding_company_logo_01_shield_512_QuZ_icon.ico" alt="" />
-                        <img className='w-10 md:w-16 borders mx-4 rounded-full' src="https://pbs.twimg.com/profile_images/712126587605491712/bmxguoTC_400x400.jpg" alt="" />
-                        <img className='w-10 md:w-16 borders mx-4 rounded-full' src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSZRHsTzZrqazDUYMHskUW4Njel3rXHPfFX68ki1Ah4qcbD-d3xelzOGC2b4NpPSciwdy0&usqp=CAU" alt="" />
-                        <img className='w-10 md:w-16 borders mx-4 rounded-full' src="https://i.pinimg.com/474x/33/42/1c/33421c607f9b651f69fef80ac3643c30--camera-aperture-photography-logo-design.jpg" alt="" />
-                        <img className='w-10 md:w-16 borders mx-4 rounded-full' src="http://hazelgraphy.com/wp-content/uploads/2015/02/Wedding_Photography_Logo_Preview.jpg" alt="" />
-                        <img className='w-10 md:w-16 borders mx-4 rounded-full hidden md:flex' src="https://i.pinimg.com/originals/29/39/e0/2939e0edff05553cdd8e85bcd5b7ba0e.png" alt="" />
-                        <img className='w-10 md:w-16 borders mx-4 rounded-full hidden md:flex' src="https://i.pinimg.com/originals/29/39/e0/2939e0edff05553cdd8e85bcd5b7ba0e.png" alt="" />
+                    <h2 className="text-4xl font-bold text-center py-5 text-cyan-700">Our Cusotmers and Wellwishers!</h2>
+                    <hr />
+                    <div className="flex justify-center p-5">
+                        <img className='w-10 md:w-16  mx-4 ' src="https://images.unsplash.com/photo-1516876437184-593fda40c7ce?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1172&q=80" alt="" />
+                        <img className='w-10 md:w-16 borders mx-4 rounded-full' src="https://images.unsplash.com/photo-1522139137660-4248e04955b8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1176&q=80" alt="" />
+                        <img className='w-10 md:w-16 borders mx-4 rounded-full' src="https://images.unsplash.com/photo-1602934445884-da0fa1c9d3b3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=958&q=80" alt="" />
+                        <img className='w-10 md:w-16 borders mx-4 rounded-full' src="https://images.unsplash.com/photo-1617727553252-65863c156eb0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80" alt="" />
+                        <img className='w-10 md:w-16 borders mx-4 rounded-full' src="https://images.unsplash.com/photo-1614851099518-055a1000e6d5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80" alt="" />
+                        <img className='w-10 md:w-16 borders mx-4 rounded-full hidden md:flex' src="https://images.unsplash.com/photo-1611488006001-eb993d4d2ec4?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=735&q=80" alt="" />
+                        <img className='w-10 md:w-16 borders mx-4 rounded-full hidden md:flex' src="https://images.unsplash.com/photo-1554463529-e27854014799?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80" alt="" />
 
 
 
