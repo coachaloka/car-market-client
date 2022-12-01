8514868b1b9da2f89798607750be80a0185780fb
Bachan-kubi <felixghagra@gmail.com>
2022-12-01 15:07:34 +0600

    payment type added

--- src/Pages/Home/HomeBanner/HomeBanner.js
+++ src/Pages/Home/HomeBanner/HomeBanner.js
@@ -6,16 +6,16 @@ const HomeBanner = () => {
       <div className="hero-content flex-col lg:flex-row-reverse">
         <img
           src="https://images.unsplash.com/photo-1485463611174-f302f6a5c1c9?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8N3x8Y2FyJTIwc2VsbHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60"
-          className="w-1/2 rounded-lg shadow-2xl"
+          className="w-1/2 rounded-lg shadow-2xl p-5 w-2/4 mt-5"
           alt="hero-img"
         />
         <div>
           <h1 className="text-4xl font-bold text-cyan-700">
-            Share your Books and Get Earned!
+            Sell and Buy your Cars and Get Earned!
           </h1>
           <p className="py-6">
-            We create a huge network to share your reading enjoyable and to earn
-            from readng by selling your book!
+            We create a huge network to share your used Cars and to earn
+            from selling your used and Cars!
           </p>
           <button className="btn btn-primary">Start Exporing!</button>
         </div>
